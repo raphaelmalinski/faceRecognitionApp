58a21fddb6f19982966a6c15acdef705a367673b
Raphael <raphaelmalinski@gmail.com>
2020-08-06 21:13:24 -0300

    Adding count number of entries functionality

--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -29,9 +29,10 @@ class Signin extends React.Component {
         })
             .then(response => response.json())
             .then(data => {
-                if (data === 'success') {
+                if(data.id){
+                    this.props.loadUser(data);
                     this.props.onRouteChange('home');
-                }
+                  }
             })
     }
 
